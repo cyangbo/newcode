bd4c23c2cf32d01d657b4c8e355f9bc68c045eee
Administrator <Administrator@192.168.1.165>
2015-07-10 16:56:18 +0800

    soap

---
+++ soap/PersonInfo.php
@@ -0,0 +1,18 @@
+<?php
+Class PersonInfo
+{
+    /**
+     *    返回姓名
+     *    @return string 
+     *
+     */
+    public function getName(){
+    	echo "sss";
+        return "My Name is Chance";
+    }
+    
+    
+}
+
+PersonInfo::getName();
+?>
